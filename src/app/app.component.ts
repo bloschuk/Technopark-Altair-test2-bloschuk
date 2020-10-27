58993ffb361bf35d9f2e5a7a712247bf5542b1d5
bloschuk <maxim.makki@gmail.com>
2020-10-27 22:32:02 +0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = "my-app";
+  firstOperand: number;
+  secondNumber: number;
+  operator: string;
+  result: string;
+  cError: string;
+  doCalc() {
+    if (
+      this.firstOperand != null ||
+      this.secondNumber != null ||
+      this.operator != null
+    ) {
+      if (this.firstOperand < this.secondNumber) {
+        this.result = "2 число больше";
+      } else if (this.firstOperand > this.secondNumber) {
+        this.result = "1 число больше";
+      } else if ((this.firstOperand = this.secondNumber)) {
+        this.result = "числа равны ";
+      }
+    } else {
+      this.result = "ввод неполный";
+    }
+  }
 }
